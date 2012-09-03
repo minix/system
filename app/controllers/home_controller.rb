ba116c6f66951fbcb13b4c3b7e651ec61c2b5986
minix <mgtuiw@gmail.com>
2012-09-03 17:21:49 +0800

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 require 'ipaddr'
+require 'socket'
+require 'digest/sha1'
+require "openssl"
+
 class HomeController < ApplicationController
   def index
 		@status = Sys.find(:all)
@@ -18,10 +22,46 @@ class HomeController < ApplicationController
 	end
 
 	def start
+		begin
+			client = TCPSocket.new('192.168.186.128', 8888)
+			#client = TCPSocket.new('0.0.0.0', 8888)
+			temp = ""
+			5.times do
+				temp << client.gets
+			end
 
+			public_key = OpenSSL::PKey::RSA.new(temp)
+			msg = 'sh /home/scripts/start_nginx.sh'
+			sha1 = Digest::SHA1.hexdigest(msg)
+			command = public_key.public_encrypt("#{sha1}*#{msg}")
+			client.send(command, 0)
+			rescue => e
+				puts e
+				retry
+			client.close
+		end
 	end
 
 	def stop
+		begin
+		  client = TCPSocket.new('192.168.186.128', 8888)
+		  temp = ""
+		  5.times do
+		    temp << client.gets
+		  end
+		
+		  public_key = OpenSSL::PKey::RSA.new(temp)
+			msg = 'sh /home/scripts/stop_nginx.sh'
+		  sha1 = Digest::SHA1.hexdigest(msg)
+
+		  command = public_key.public_encrypt("#{sha1}*#{msg}")
+		  client.send(command,0)
+		rescue => e
+		  puts e
+		  retry
+		end
+		
+		client.close
 
 	end
 

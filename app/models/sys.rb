b0665f72aaa2ccb30782949eccbd3f36add47dfa
minix <mgtuiw@gmail.com>
2012-09-19 23:41:36 +0800

    update model

--- app/models/sys.rb
+++ app/models/sys.rb
@@ -1,4 +1,14 @@
 class Sys < ActiveRecord::Base
 	belongs_to :ip
 	attr_accessible :server, :port, :oid
+	
+	before_save :oid
+
+	private
+
+	def oid
+		custom_oid = ('0'..'9').to_a.shuffle[0..3].join
+		self.oid = ".1.3.6.1.4.2021.#{custom_oid}"
+	end
+
 end

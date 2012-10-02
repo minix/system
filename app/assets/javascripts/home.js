8e8259a9ac2bb7b3f35845e5b4efa29889dd7456
minix <mgtuiw@gmail.com>
2012-10-02 16:25:43 +0800

    change index show

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,7 @@
+function toggle_visible (elName) {
+	var el = document.getElementById(elName);
+	var isVisible = (el.style.visibility == "hidden") ? true : false;
+
+	el.style.visibility = isVisible ? "visible" : "hidden";
+	el.style.display = isVisible ? "inline" : "none";
+}
